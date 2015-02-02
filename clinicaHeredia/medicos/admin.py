aad521959441297d49654cbb09e4d988c24ce6cb
Fabian Araya <josefabian2588@gmail.com>
2015-02-02 19:36:52 -0300

    cambios ofi

--- clinicaHeredia/medicos/admin.py
+++ clinicaHeredia/medicos/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Medico
+from actions import export_as_excel
+from actions import export_as_csv_action
+
+class MedicoAdmin(admin.ModelAdmin):
+	list_display = ('codigoMedico_med' ,)
+	list_filter =('codigoMedico_med',)
+	search_fields = ('numeroCarnet_pac',)
+	actions = (export_as_excel,)
+
+
+
 
 admin.site.register(Medico)
--- clinicaHeredia/pacientes/admin.py
+++ clinicaHeredia/pacientes/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Paciente
+from actions import export_as_excel
+from actions import export_as_csv_action
 
-admin.site.register(Paciente)
+
+
+class PacienteAdmin(admin.ModelAdmin):
+	list_display = ('numeroCarnet_pac' ,'Enfermedad_pac' ,'comentario_pac',)
+	list_filter =('numeroCarnet_pac','Enfermedad_pac',)
+	search_fields = ('numeroCarnet_pac',)
+	actions = (export_as_excel,)
+
+
+
+admin.site.register(Paciente,PacienteAdmin)
